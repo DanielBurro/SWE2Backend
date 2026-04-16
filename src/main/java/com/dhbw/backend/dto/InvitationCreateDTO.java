97db3eec889c1479eaab2122ded4387d7835be63
daburrow <daniel.a.burrows@capgemini.com>
2026-04-16 19:04:02 +0200

    Implement invitation management features: add methods for retrieving invitations by ID and event with optional status filtering; enhance DTOs with validation for guest ID and plus ones; add invitation deletion functionality.

--- src/main/java/com/dhbw/backend/dto/InvitationCreateDTO.java
+++ src/main/java/com/dhbw/backend/dto/InvitationCreateDTO.java
@@ -1,10 +1,15 @@
 package com.dhbw.backend.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class InvitationCreateDTO {
     // Um jemanden einzuladen
+
+    @NotNull(message = "Event-ID darf nicht null sein.")
     private Long eventId;
+
+    @NotNull(message = "Gast-ID darf nicht null sein.")
     private Long guestId;
 }
