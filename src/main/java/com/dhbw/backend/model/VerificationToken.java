052eee0377c29d46b676bb244c024a698a81be22
Daniel Burrows <inf24164@lehre.dhbw-stuttgart.de>
2026-04-23 10:18:52 +0200

    Fix column name for user ID in VerificationToken model

--- src/main/java/com/dhbw/backend/model/VerificationToken.java
+++ src/main/java/com/dhbw/backend/model/VerificationToken.java
@@ -13,18 +13,22 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class VerificationToken {
 
+    // 33. Verifizierungs-ID
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "TOKEN_ID")
     private Long id;
 
+    // 35. Verifizierungs-Token
     @Column(name = "TOKEN_VALUE", nullable = false, unique = true)
     private String token;
 
+    // 34. User-ID
     @OneToOne(targetEntity = Users.class, fetch = FetchType.EAGER)
-    @JoinColumn(nullable = false, name = "USER_ID")
+    @JoinColumn(nullable = false, name = "TOKEN_USER_ID")
     private Users user;
 
+    // 35. Ablaufdatum
     @Column(name = "EXPIRY_DATE", nullable = false)
     private LocalDateTime expiryDate;
 
